c749a6d733fd11e5909e6ef62079432b3a9fc3bb
Bao Hoang <bao15899@yahoo.com>
2022-05-23 14:06:01 +0700

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Toi Nguyen on 23/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
